6537429e44cdf0cc04db4479a020af55776b82a7
Pratham Bhoraniya <Pratham.Bhoraniya@4cpl.com>
2026-05-08 13:57:56 +0530

    Add vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -7,29 +7,33 @@ import Projects from "./components/Projects";
 import Testimonials from "./components/Testimonials";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import { Analytics } from "@vercel/analytics/next";
 import "./App.css";
 
 function App() {
   return (
-    <div className="app">
-      <Navbar />
-      <main className="flex-1">
-        <Hero />
-        <div className="container-main h-px bg-border-subtle" />
-        <About />
-        <div className="container-main h-px bg-border-subtle" />
-        <Skills />
-        <div className="container-main h-px bg-border-subtle" />
-        <Experience />
-        {/* <div className="container-main h-px bg-border-subtle" />
+    <>
+      <div className="app">
+        <Navbar />
+        <main className="flex-1">
+          <Hero />
+          <div className="container-main h-px bg-border-subtle" />
+          <About />
+          <div className="container-main h-px bg-border-subtle" />
+          <Skills />
+          <div className="container-main h-px bg-border-subtle" />
+          <Experience />
+          {/* <div className="container-main h-px bg-border-subtle" />
         <Projects /> */}
-        {/* <div className="container-main h-px bg-border-subtle" />
+          {/* <div className="container-main h-px bg-border-subtle" />
         <Testimonials /> */}
-        <div className="container-main h-px bg-border-subtle" />
-        <Contact />
-      </main>
-      <Footer />
-    </div>
+          <div className="container-main h-px bg-border-subtle" />
+          <Contact />
+        </main>
+        <Footer />
+      </div>
+      <Analytics />
+    </>
   );
 }
 
